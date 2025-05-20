1919655ba9303f5a4d1021607e1fc2d04c88c30c
Bartosz <Bartekszkolny12@gmail.com>
2025-05-20 22:06:07 +0200

    update

--- public/index.js
+++ public/index.js
@@ -6,3 +6,12 @@ document.getElementById('kontoBtn').addEventListener('click', function () {
     window.location.href = '/uzytkownikkonto/index.html';
   }
 });
+document.getElementById('losowanieBtn').addEventListener('click', function () {
+  const userId = localStorage.getItem('user_id');
+
+  if (!userId) {
+    alert('Musisz być zalogowany, aby przejść do losowania.');
+  } else {
+    window.location.href = '/podstrona1/index.html';
+  }
+});
